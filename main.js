1c0c3a2d39e4cbf606fcce9141cd2ff631fabca7
battlefis <95596872+battlefis@users.noreply.github.com>
2022-08-19 19:59:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("My first console project");
+
+var first_name = "sahith";
+
+
+var last_name = "Aaloori";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name)
+var a = 10;
+
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
